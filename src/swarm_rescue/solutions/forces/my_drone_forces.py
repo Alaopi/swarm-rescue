8ecf98d39b77c801c2c84614a6fb93c1a037d3d0
NicolasBnf <105448952+NicolasBnf@users.noreply.github.com>
2023-01-21 15:43:43 +0100

    Class enum for mapping
    Class enum for maping

--- src/swarm_rescue/solutions/forces/my_drone_forces.py
+++ src/swarm_rescue/solutions/forces/my_drone_forces.py
@@ -12,29 +12,32 @@ from spg_overlay.entities.drone_abstract import DroneAbstract
 from spg_overlay.utils.misc_data import MiscData
 from spg_overlay.utils.utils import normalize_angle
 
+
 class Vector:
-    def __init__(self, amplitude = -1, arg = 'a', x = 'a', y = 'a' ):
+    def __init__(self, amplitude=-1, arg='a', x='a', y='a'):
         if amplitude == -1 or arg == 'a':
             self.x = x
             self.y = y
-        elif(x == 'a' or y == 'a'):
+        elif (x == 'a' or y == 'a'):
             self.x = amplitude*np.cos(arg)
             self.y = self.amplitude*np.sin(arg)
 
-    def add_vector(self,v1):
+    def add_vector(self, v1):
         self.x += v1.x
         self.y += v1.y
-            
+
 
 class MyForceDrone(DroneAbstract):
     def __init__(self,
                  identifier: Optional[int] = None,
                  misc_data: Optional[MiscData] = None,
                  **kwargs):
+
         super().__init__(identifier=identifier,
                          misc_data=misc_data,
                          should_display_lidar=False,
                          **kwargs)
+        self.drone_map = np.zeros(self.size_area)
 
     def define_message_for_all(self):
         """
@@ -42,8 +45,7 @@ class MyForceDrone(DroneAbstract):
         """
         pass
 
-
-    class Activity(Enum): ##Possible values of self.state which gives the current action of the drone
+    class Activity(Enum):  # Possible values of self.state which gives the current action of the drone
         """
         All the states of the drone as a state machine
         """
@@ -64,82 +66,93 @@ class MyForceDrone(DroneAbstract):
 
 ################### ALEX ##########################
 
-    
-
 
 ################### END ###########################
 
 ################### ILIAS #########################
-   
-    #Definition of the various forces used to control the drone
+
+    # Definition of the various forces used to control the drone
+
     def wall_force(distance, angle):
 
         amplitude = 1
-        f = Vector(amplitude, angle-np.pi)#repulsive : angle-Pi
+        f = Vector(amplitude, angle-np.pi)  # repulsive : angle-Pi
         return f
 
     def drone_force(distance, angle):
         amplitude = 1
-        f = Vector(amplitude, angle-np.pi)#repulsive : angle-pi
+        f = Vector(amplitude, angle-np.pi)  # repulsive : angle-pi
         return f
 
     def wounded_force(distance, angle):
         amplitude = 1
-        f = Vector(amplitude, angle) #attractive : angle
+        f = Vector(amplitude, angle)  # attractive : angle
         return f
 
     def total_force(self, detection_semantic):
         forces = []
         for data in detection_semantic:
-            match data.entity_type :
-                case MyForceDrone.TypeEntity.WOUNDED_PERSON :
-                    ##if the detected person is not grabbed, we are attracted by it, otherwise there is another drone that creates a repulsive force
+            match data.entity_type:
+                case MyForceDrone.TypeEntity.WOUNDED_PERSON:
+                    # if the detected person is not grabbed, we are attracted by it, otherwise there is another drone that creates a repulsive force
                     if not data.grasped and self.state is MyForceDrone.Activity.SEARCHING_WOUNDED:
-                        forces.append(self.wounded_force(data.distance, data.angle))
-                    else :
-                        forces.append(self.drone_force(data.distance, data.angle))
+                        forces.append(self.wounded_force(
+                            data.distance, data.angle))
+                    else:
+                        forces.append(self.drone_force(
+                            data.distance, data.angle))
 
-                case MyForceDrone.TypeEntity.RESCUE_CENTER :
-                    #if we are looking for a wounded person, the rescue center is considered a wall (repulsive force)
+                case MyForceDrone.TypeEntity.RESCUE_CENTER:
+                    # if we are looking for a wounded person, the rescue center is considered a wall (repulsive force)
                     if self.state is not MyForceDrone.Activity.DROPPING_AT_RESCUE_CENTER:
-                        forces.append(self.wall_force(data.distance, data.angle))
+                        forces.append(self.wall_force(
+                            data.distance, data.angle))
 
-                case MyForceDrone.TypeEntity.WALL : 
+                case MyForceDrone.TypeEntity.WALL:
                     pass
 
-                case MyForceDrone.TypeEntity.DRONE : 
+                case MyForceDrone.TypeEntity.DRONE:
                     forces.append(self.drone_force(data.distance, data.angle))
 
-        total_force = Vector(x=0,y=0)
+        total_force = Vector(x=0, y=0)
         for force in forces:
             total_force.add_vector(force)
         return total_force
-        
-
 
 
 ################### END ###########################
 
 ################### Nicolas #########################
 
+    class MapState(Enum):
+        """ State of each elements of the matrix representing the map
+        """
+        UNKNOWN = 0
+        WALL = 1
+        EMPTY = 2
+        WOUNDED = 3
+        INIT_RESCUE = 4
+        KILL_ZONE = 5
+        NO_GPS = 6
+        NO_COM = 7
 
     def update_map(self, detection_semantic):
+
         pass
 
 ################### END ###########################
-
 
     def control(self):
         """
         The drone will behave differently according to its current state
         """
         command = {"forward": 0,
-                        "lateral": 0,
-                        "rotation ": 0, ##We try to align the force and the front side of the drone
-                        "grasper": 0}
+                   "lateral": 0,
+                   "rotation ": 0,  # We try to align the force and the front side of the drone
+                   "grasper": 0}
 
         detection_semantic = self.semantic.get_sensor_values()
-        
+
         ##TODO : share_map
         ##TODO : receive_maps
         ##TODO : update_map(detection_semantic)
@@ -147,12 +160,14 @@ class MyForceDrone(DroneAbstract):
             case MyForceDrone.Activity.SEARCHING_WOUNDED:
                 force = self.total_force()
                 forward_force = force.x
-                lateral_force = force.y #To check : +PI/2 should be right of the drone, and right should be lateral>0
-                
-                command = {"forward": min(forward_force,1) if forward_force > 0 else max(forward_force,-1),
-                        "lateral": min(lateral_force,1) if lateral_force > 0 else max(lateral_force,-1),
-                        "rotation ": np.sign(lateral_force)/2, ##We try to align the force and the front side of the drone
-                        "grasper": 0}
+                # To check : +PI/2 should be right of the drone, and right should be lateral>0
+                lateral_force = force.y
+
+                command = {"forward": min(forward_force, 1) if forward_force > 0 else max(forward_force, -1),
+                           "lateral": min(lateral_force, 1) if lateral_force > 0 else max(lateral_force, -1),
+                           # We try to align the force and the front side of the drone
+                           "rotation ": np.sign(lateral_force)/2,
+                           "grasper": 0}
 
             case MyForceDrone.Activity.GRASPING_WOUNDED:
                 pass
